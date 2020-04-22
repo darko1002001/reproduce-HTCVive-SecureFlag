826fec6456453ef99b929570579f0421694987e6
Darko Grozdanovski <dgrozdanovski@mobileiron.com>
2020-04-22 10:02:28 +0200

    initial commit

---
+++ app/src/main/java/com/dg/examples/issuereproduceactivity/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.dg.examples.issuereproduceactivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE)
+        setContentView(R.layout.activity_main)
+    }
+}
